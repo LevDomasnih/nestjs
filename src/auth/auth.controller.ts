1d66f34ad93a8f3500412cca8b9d7ee5b58b54d5
lev <lev.domasnix2013@gmail.com>
2021-09-08 23:23:53 +0500

    Авторизация и генерация JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import {AuthDto} from './dto/auth.dto';
+import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { ALREADY_REGISTERED_ERROR } from './auth.constans';
 
@@ -17,9 +17,11 @@ export class AuthController {
 		return this.authService.createUser(dto)
 	}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
-	async login(@Body() dto: AuthDto) {
-
+	async login(@Body() {login, password}: AuthDto) {
+		const { email } = await this.authService.validateUser(login, password)
+		return this.authService.login(email)
 	}
 }
