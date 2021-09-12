8821f8fe084aa37e317ff5c8551b490e8686f1f9
lev <lev.domasnix2013@gmail.com>
2021-09-12 15:49:32 +0500

    Pipe для валидации id

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -14,7 +14,8 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constans';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
-import { UserEmail } from '../auth/decorators/user-email.decorator';
+import { UserEmail } from '../decorators/user-email.decorator';
+import { IdValidationPipe } from '../pipes/id-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
@@ -28,7 +29,7 @@ export class ReviewController {
 
 	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedDoc = await this.reviewService.delete(id)
 		if (!deletedDoc) {
 			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
@@ -36,7 +37,7 @@ export class ReviewController {
 	}
 
 	@Get('byProduct/:productId')
-	async get(@Param('productId') productId: string, @UserEmail() email: string) {
+	async get(@Param('productId', IdValidationPipe) productId: string, @UserEmail() email: string) {
 		return this.reviewService.findByProductId(productId)
 	}
 }
