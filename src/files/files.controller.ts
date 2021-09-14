c8776b7936cf776a909adc9493f51b0e55086654
lev <lev.domasnix2013@gmail.com>
2021-09-15 00:13:29 +0500

    Конвертация изображений

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { FileElementResponse } from './dto/file-element.response';
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 
 @Controller('files')
 export class FilesController {
@@ -13,6 +14,17 @@ export class FilesController {
 	@UseGuards(JwtAuthGuard)
 	@UseInterceptors(FileInterceptor('files'))
 	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
-		return this.fileService.saveFile([file])
+		const saveArray: MFile[] = [new MFile(file)]
+
+		if (file.mimetype.includes('image')) {
+			const buffer = await this.fileService.convertToWebP(file.buffer)
+
+			saveArray.push(new MFile({
+				originalname: `${file.originalname.split('.')[0]}.webp`,
+				buffer
+			}))
+		}
+
+		return this.fileService.saveFile(saveArray)
 	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,11 +3,13 @@ import { FileElementResponse } from './dto/file-element.response';
 import { format } from 'date-fns'
 import { path } from 'app-root-path'
 import { ensureDir, writeFile } from 'fs-extra'
+import * as sharp from 'sharp';
+import { MFile } from './mfile.class';
 
 @Injectable()
 export class FilesService {
 
-	async saveFile(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
+	async saveFile(files: MFile[]): Promise<FileElementResponse[]> {
 		const dateFolder = format(new Date(), 'yyyy-MM-dd')
 		const uploadFolder = `${path}/uploads/${dateFolder}`
 		await ensureDir(uploadFolder)
@@ -19,5 +21,11 @@ export class FilesService {
 		}
 
 		return res
+	}
+
+	async convertToWebP(file: Buffer): Promise<Buffer> {
+		return sharp(file)
+			.webp()
+			.toBuffer()
 	}
 }
