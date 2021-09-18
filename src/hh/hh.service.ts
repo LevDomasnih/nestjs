9403fcaa6fccc3a6594812d260b4ebe428198518
lev <lev.domasnix2013@gmail.com>
2021-09-18 23:34:24 +0500

    Начал разбираться с HttpModule

--- src/hh/hh.service.ts
+++ src/hh/hh.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { API_URL } from './hh.constans';
 
 @Injectable()
-export class HhService {}
+export class HhService {
+	token: string
+
+	constructor(
+		private readonly configService: ConfigService,
+		private readonly httpService: HttpService
+	) {
+		this.token = this.configService.get('HH_TOKEN') ?? ''
+	}
+
+	async getData(text: string) {
+		this.httpService.get(API_URL.vacancies, {
+			params: {
+				text,
+				clusters: true
+			},
+			headers: {
+				'User-Agent': 'OwlTop/1.0 (Lev@gmail.com)',
+				Authorization: 'Bearer ' + this.token
+			}
+		})
+	}
+}
